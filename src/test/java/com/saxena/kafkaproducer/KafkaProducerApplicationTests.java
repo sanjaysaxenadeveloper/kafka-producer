5d15fbdaba395e5206da64970abb794612089ccc
Sanjay Saxena <sanjaysaxenadeveloper@gmail.com>
2024-05-08 19:52:37 +0530

    Initial commit

---
+++ src/test/java/com/saxena/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxena.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
